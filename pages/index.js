66504a914a91a892a2cfd0fda0add19c1fd7ccd6
Morio <55315690+moriotagami@users.noreply.github.com>
2020-09-24 14:08:47 +0900

    トップマークアップ（PCのみ）

--- pages/index.js
+++ pages/index.js
@@ -20,21 +20,29 @@ export default function Home() {
             </h1>
             <p>ミーティングのアジェンダが、プロジェクトをスムースに</p>
           </div>
+
           <div className={styles.service_about}>
-            <div className={styles.content_alignleft}>
-              <img
-                src="/images/sgms_screenshot1.png"
-                alt="SuperGoodMeetingsのスクリーンショット。トラックが表示されている"
-              ></img>
-              <h2>
-                メンバーの持つちからを引き出し、 プロジェクトをより速く、
-                <br />
-                本質的に成功させたい。
-              </h2>
-              <p>
-                そう考えているチームのために、ミーティング活用クラウドサービス
-                SuperGoodMeetingsは開発されました。
-              </p>
+            <div className={styles.story1}>
+              <figure>
+                <img
+                  src="/images/sgms_screenshot1.png"
+                  alt="SuperGoodMeetingsのスクリーンショット。トラックが表示されている"
+                ></img>
+              </figure>
+              <div className={styles.content}>
+                <h2>
+                  メンバーの持つちからを引き出し、
+                  <br />
+                  プロジェクトをより速く、
+                  <br />
+                  本質的に成功させたい。
+                </h2>
+                <p>
+                  そう考えているチームのために、ミーティング活用クラウド
+                  <br />
+                  サービス SuperGoodMeetingsは開発されました。
+                </p>
+              </div>
             </div>
             <div className={styles.bubbles}>
               <div className={styles.bubble}>
@@ -43,7 +51,10 @@ export default function Home() {
                   alt="プロジェクトのイメージイラスト"
                 ></img>
                 <h3>こんなプロジェクトに</h3>
-                <p>不確実性の高い環境・状況 複雑なアウトプット</p>
+                <p>
+                  不確実性の高い環境・状況 <br />
+                  複雑なアウトプット
+                </p>
               </div>
               <div className={styles.bubble}>
                 <img
@@ -51,111 +62,163 @@ export default function Home() {
                   alt="チームのイメージイラスト"
                 ></img>
                 <h3>こんなチームに</h3>
-                <p>部門/組織横断のチーム 多様性のあるメンバー</p>
+                <p>
+                  部門/組織横断のチーム
+                  <br /> 多様性のあるメンバー
+                </p>
               </div>
             </div>
-            <div className={styles.content_alignright}>
-              <h2>
-                みんなでプロジェクトを <br />
-                ファシリテーションしよう。
-              </h2>
-              <p>
-                メンバー全員で推進することが、
-                不確実性の高いプロジェクトを成功させる近道です。
-              </p>
-              <img
-                src="/images/sgms_screenshot2.png"
-                alt="SuperGoodMeetingsのスクリーンショット。ミーティングとアジェンダのリストが表示されている"
-              ></img>
+            <div className={styles.story2}>
+              <div className={styles.content}>
+                <h2>
+                  みんなでプロジェクトを <br />
+                  ファシリテーションしよう。
+                </h2>
+                <p>
+                  メンバー全員で推進することが、
+                  <br />
+                  不確実性の高いプロジェクトを成功させる近道です。
+                </p>
+              </div>
+              <figure>
+                <img
+                  src="/images/sgms_screenshot2.png"
+                  alt="SuperGoodMeetingsのスクリーンショット。ミーティングとアジェンダのリストが表示されている"
+                ></img>
+              </figure>
             </div>
           </div>
+
           <div className={styles.service_effect}>
             <h2>サービス導入による効果</h2>
             <ul className={styles.items}>
               <li>
-                <img
-                  src="/images/illust_effect1.svg"
-                  alt="イメージイラスト"
-                ></img>
+                <figure>
+                  <img
+                    src="/images/illust_effect1.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
                 定例ミーティングの実施によってリズムが生まれ、プロジェクトを計画しやすくなります。
               </li>
               <li>
-                <img
-                  src="/images/illust_effect2.svg"
-                  alt="イメージイラスト"
-                ></img>
+                <figure>
+                  <img
+                    src="/images/illust_effect2.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
                 定期的に試しながら進めるため、フィードバックを得ながらプロジェクトを実行できます。
               </li>
               <li>
-                <img
-                  src="/images/illust_effect3.svg"
-                  alt="イメージイラスト"
-                ></img>
+                <figure>
+                  <img
+                    src="/images/illust_effect3.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
                 アジェンダをみんなで提案するので、プロジェクトにおける問題意識を共有できます。
               </li>
               <li>
-                <img
-                  src="/images/illust_effect4.svg"
-                  alt="イメージイラスト"
-                ></img>
+                <figure>
+                  <img
+                    src="/images/illust_effect4.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
                 プロジェクト全体のマイルストーンを常に意識でき、アップデートしやすくなります。
               </li>
             </ul>
           </div>
+
           <div className={styles.service_change}>
             <h2>サービス導入後の変化</h2>
             <ul className={styles.items}>
-              <li>Projects</li>
               <li>
-                <img
-                  src="/images/illust_change1.svg"
-                  alt="イメージイラスト"
-                ></img>
-                変化を前提とし、 柔軟に対応できるように なります。
+                <figure>
+                  <img
+                    src="/images/illust_change_project.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
               </li>
               <li>
-                <img
-                  src="/images/illust_change2.svg"
-                  alt="イメージイラスト"
-                ></img>
-                本質的な問題解決に向けて 進化しやすくなります。
+                <figure>
+                  <img
+                    src="/images/illust_change1.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
+                変化を前提とし、 <br />
+                柔軟に対応できるように <br />
+                なります。
               </li>
               <li>
-                <img
-                  src="/images/illust_change3.svg"
-                  alt="イメージイラスト"
-                ></img>
-                課題や変化の兆しを 発見しやすくなります。
+                <figure>
+                  <img
+                    src="/images/illust_change2.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
+                本質的な問題解決に向けて <br /> 進化しやすくなります。
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/images/illust_change3.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
+                課題や変化の兆しを <br /> 発見しやすくなります。
               </li>
             </ul>
             <ul className={styles.items}>
-              <li>Team</li>
               <li>
-                <img
-                  src="/images/illust_change4.svg"
-                  alt="イメージイラスト"
-                ></img>
-                進める際に最適な アプローチを 考えやすくなります。
+                <figure>
+                  <img
+                    src="/images/illust_change_team.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
               </li>
               <li>
-                <img
-                  src="/images/illust_change5.svg"
-                  alt="イメージイラスト"
-                ></img>
-                自律的にプロジェクトを 推進できるようになります。
+                <figure>
+                  <img
+                    src="/images/illust_change4.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
+                進める際に最適な <br />
+                アプローチを <br /> 考えやすくなります。
               </li>
               <li>
-                <img
-                  src="/images/illust_change6.svg"
-                  alt="イメージイラスト"
-                ></img>
-                メンバー全員での改善が 行われやすくなります。
+                <figure>
+                  <img
+                    src="/images/illust_change5.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
+                自律的にプロジェクトを <br />
+                推進できるようになります。
+              </li>
+              <li>
+                <figure>
+                  <img
+                    src="/images/illust_change6.svg"
+                    alt="イメージイラスト"
+                  ></img>
+                </figure>
+                メンバー全員での改善が <br />
+                行われやすくなります。
               </li>
             </ul>
           </div>
+
           <div className={styles.service_detail}>
             <div className={styles.pgs}>
-              <img src="/images/pjs_logo.png" alt="Project Sprintロゴ"></img>
+              <figure>
+                <img src="/images/pjs_logo.png" alt="Project Sprintロゴ"></img>
+              </figure>
               <p>
                 「SuperGoodMeetings」はプロジェクト推進メソッド「プロジェクトスプリント」にもとづいて設計されています。
                 <br />
@@ -176,41 +239,63 @@ export default function Home() {
               </p>
             </div>
           </div>
+
           <div className={styles.movie}>
             <h2>
               動画でわかる、<span>SuperGoodMeetings</span>
             </h2>
             <div className={styles.dummymovie}></div>
           </div>
+
           <div className={styles.contact}>
             <div className={styles.user_community}>
-              <img
-                src="/images/illust_usercommunity.svg"
-                alt="ユーザーコミュニティのイラスト"
-              ></img>
+              <figure>
+                <img
+                  src="/images/illust_usercommunity.svg"
+                  alt="ユーザーコミュニティのイラスト"
+                ></img>
+              </figure>
               <h2>User Community</h2>
-              <p>
-                プロジェクト推進メソッド Project
-                Sprintやミーティング活用クラウドサービス
-                SuperGoodMeetingsについて、ちょっとした質問や情報交換ができるユーザーコミュニティをSlackにて運営しています。
+              <div className={styles.text}>
+                <p>
+                  プロジェクト推進メソッド Project
+                  Sprintやミーティング活用クラウドサービス
+                  SuperGoodMeetingsについて、ちょっとした質問や情報交換ができるユーザーコミュニティをSlackにて運営しています。
+                </p>
+                <p>
+                  メソッドやサービスの最新情報のキャッチアップや、使い方・不明点に関するご相談、開発メンバーとのコミュニケーションなど、ぜひお気軽にご活用ください！
+                </p>
+              </div>
+              <div className={styles.button} role="button">
+                <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfc2sXdcxDEz5Boi08Vymj7EtGeZAsCSljg6bBB0g1d9Aea0g/viewform">
+                  今すぐ登録！
+                </Link>
+              </div>
+              <p className={styles.caption}>
+                <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfc2sXdcxDEz5Boi08Vymj7EtGeZAsCSljg6bBB0g1d9Aea0g/viewform">
+                  すでに登録している方はこちら
+                </Link>
               </p>
-              <p>
-                メソッドやサービスの最新情報のキャッチアップや、使い方・不明点に関するご相談、開発メンバーとのコミュニケーションなど、ぜひお気軽にご活用ください！
-              </p>
-              <button>今すぐ登録</button>
-              <p>すでに登録している方はこちら</p>
             </div>
-            <div className={styles.contactus}>
-              <img
-                src="/images/illust_contactus.svg"
-                alt="コンタクトのイラスト"
-              ></img>
+            <div className={styles.contact_us}>
+              <figure>
+                <img
+                  src="/images/illust_contactus.svg"
+                  alt="コンタクトのイラスト"
+                ></img>
+              </figure>
               <h2>Contact us</h2>
-              <p>
-                SuperGoodMeetings
-                クローズドβ版について、サービス詳細や本ツールを用いたプロジェクト推進サポートにご興味のある方は、こちらよりご連絡ください。
-              </p>
-              <button>お問い合わせ</button>
+              <div className={styles.text}>
+                <p>
+                  SuperGoodMeetings
+                  クローズドβ版について、サービス詳細や本ツールを用いたプロジェクト推進サポートにご興味のある方は、こちらよりご連絡ください。
+                </p>
+              </div>
+              <div className={styles.button} role="button">
+                <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfc2sXdcxDEz5Boi08Vymj7EtGeZAsCSljg6bBB0g1d9Aea0g/viewform">
+                  お問い合わせ
+                </Link>
+              </div>
             </div>
           </div>
         </div>
