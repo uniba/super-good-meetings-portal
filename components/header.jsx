8ca8eb19169c876d712cce12deec5cafe59b7f0d
hitsujiwool <utatanenohibi@gmail.com>
2020-10-06 12:28:18 +0900

    ログインリンクのドメインをsgms.appに修正

--- components/header.jsx
+++ components/header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       </Link>
       <ul className={styles.sessions}>
         <li>
-          <Link href="https://supergoodmeetings.com/login">LOG IN</Link>
+          <Link href="https://sgms.app/login">LOG IN</Link>
         </li>
         <li>
           <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfc2sXdcxDEz5Boi08Vymj7EtGeZAsCSljg6bBB0g1d9Aea0g/viewform">
