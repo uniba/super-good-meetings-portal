25f5613c92d24c5aea72575d1848df20e897be61
Morio <55315690+moriotagami@users.noreply.github.com>
2020-11-05 12:17:14 +0900

    Google Tag Manager 埋め込み

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,13 @@ class MyDocument extends Document {
   }
 
   render() {
+    const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KQJWBQH')`;
+    const gtmFrame = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQJWBQH"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
     return (
       <Html>
         <Head>
@@ -276,8 +283,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700,900|Lato:400,900|Open+Sans:400,700,700i,800,800i&display=swap"
             rel="stylesheet"
           />
+          <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{ __html: gtmFrame }} />
           <Main />
           <NextScript />
         </body>
