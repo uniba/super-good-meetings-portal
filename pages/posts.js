ce8ac373a6bcdb284fde8ee71979e091969b4955
Morio <55315690+moriotagami@users.noreply.github.com>
2021-01-25 01:13:08 +0900

    トップ/お知らせページ整形

--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router';
+import Head from "next/head";
+import Layout from "../components/layout";
 import Link from 'next/link';
+import styles from "./styles/news.scss";
 import { chunk } from 'lodash';
 import { getAllPosts } from '../lib/api';
 
@@ -9,31 +12,38 @@ export default function Posts ({ allPosts }) {
   const pages = chunk(allPosts, 10)
   const posts = pages[currentPage - 1]
   return (
-    <div>
-      <h1>お知らせ</h1>
-      <div>
-        {posts.map((post, i) => (
-          <div key={i}>
-            <p>
-              <span>{post.date}</span>
-              <Link href={`/posts/${encodeURIComponent(post.slug)}`}>
-                {post.title}
-              </Link>
-            </p>
+    <>
+      <Head>
+        <title>プライバシーポリシー - SuperGoodMeetings</title>
+      </Head>
+      <Layout>
+        <div className={styles.posts_container}>
+          <h1>お知らせ</h1>
+          <div>
+            {posts.map((post, i) => (
+              <div key={i}>
+                <p className={styles.posts_item}>
+                  <span>{post.date}</span>
+                  <Link href={`/posts/${encodeURIComponent(post.slug)}`}>
+                    {post.title}
+                  </Link>
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <ul>
-        {pages.map((_, i) => {
-          const page = i + 1;
-          if (page === currentPage) {
-            return <li key={page}>{page}</li>;
-          } else {
-            return <li key={page}><Link href={{ pathname: '/posts', query: { page } }}>{`${page}`}</Link></li>;
-          }
-        })}
-      </ul>
-    </div>
+          <ul className={styles.pager}>
+            {pages.map((_, i) => {
+              const page = i + 1;
+              if (page === currentPage) {
+                return <li key={page}>{page}</li>;
+              } else {
+                return <li key={page}><Link href={{ pathname: '/posts', query: { page } }}>{`${page}`}</Link></li>;
+              }
+            })}
+          </ul>
+        </div>
+      </Layout>
+    </>
   );
 }
 
