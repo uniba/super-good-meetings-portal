55561b8b14666e1adbf1a0341a2d3de642206349
monpy <4696monpy@gmail.com>
2022-07-14 12:52:53 +0900

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -18,11 +18,6 @@ const Footer = () => {
               <Link href="/#about">
                 <a id="ga_about_footer">サービス紹介</a>
               </Link>
-              <Link href="/src/SuperGoodMeetings_introduction.pdf">
-                <a id="ga_intropdf_footer"  target="_blank" className={styles.footer_sublink}>
-                  PDF版ダウンロード
-                </a>
-              </Link>
             </li>
             <li>
               <Link href="/plan">
