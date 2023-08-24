869951c688f8896a6b3194cbd22f8c7ef3957b57
Itsuki-Yoshizawa <itsuki@uniba.jp>
2023-08-24 09:58:39 +0900

    ディレクトリ名・関数名 変更

--- lib/api.ts
+++ lib/api.ts
@@ -4,8 +4,8 @@ import matter from "gray-matter";
 
 const DIRECTORIES = {
   posts: join(process.cwd(), "_posts"),
-  releases: join(process.cwd(), "_releases"),
-  achievements: join(process.cwd(), "_achievements"),
+  releaseNotes: join(process.cwd(), "_release_notes"),
+  news: join(process.cwd(), "_news"),
 };
 
 function getSlugs(directory: string): string[] {
@@ -50,14 +50,14 @@ export function getAll(directory: string, fields: string[] = []): any[] {
 
 export const getPostBySlug = (slug: string, fields: string[] = []) =>
   getBySlug(DIRECTORIES.posts, slug, fields);
-export const getReleaseBySlug = (slug: string, fields: string[] = []) =>
-  getBySlug(DIRECTORIES.releases, slug, fields);
-export const getAchievementBySlug = (slug: string, fields: string[] = []) =>
-  getBySlug(DIRECTORIES.achievements, slug, fields);
+export const getReleaseNotesBySlug = (slug: string, fields: string[] = []) =>
+  getBySlug(DIRECTORIES.releaseNotes, slug, fields);
+export const getNewsBySlug = (slug: string, fields: string[] = []) =>
+  getBySlug(DIRECTORIES.news, slug, fields);
 
 export const getAllPosts = (fields: string[] = []) =>
   getAll(DIRECTORIES.posts, fields);
-export const getAllReleases = (fields: string[] = []) =>
-  getAll(DIRECTORIES.releases, fields);
-export const getAllAchievements = (fields: string[] = []) =>
-  getAll(DIRECTORIES.achievements, fields);
+export const getAllReleaseNotes = (fields: string[] = []) =>
+  getAll(DIRECTORIES.releaseNotes, fields);
+export const getAllNews = (fields: string[] = []) =>
+  getAll(DIRECTORIES.news, fields);
