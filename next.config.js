fb8e3ee9c5c6705243e89be91a802c05c31b1660
Itsuki-Yoshizawa <itsuki@uniba.jp>
2023-08-31 09:22:03 +0900

    リダイレクト処理 削除
    - postsの中にリリースノートの記事もあるため、postsはpostsのままにしておく

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
-module.exports = {
-  /* ここにオプション設定を書きます */
-  async redirects() {
-    return [
-      {
-        source: "/posts/:path*",
-        destination: "/news/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+module.exports = {};
